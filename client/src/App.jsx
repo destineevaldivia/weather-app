9a6fe3445250071bb0195a7fcf713e2c217cfca5
destineevaldivia <destvaldivia@gmail.com>
2023-10-12 15:45:29 -0700

    restructuring components such that App will just be responsible for rendering two child components and moving the state down to where weatherForm component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,13 @@
-import WeatherCard from './components/WeatherCard';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import WeatherCard from "./components/WeatherCard";
+import WeatherForm from "./components/weatherForm";
 
 function App() {
-  const [city, setCity] = useState('');
-  const [result, setResult] = useState(null);
-    
-//fetch weather data from BE 
-const loadCity = async (city) => {
-  console.log(city);
-  const params = new URLSearchParams({ cityName: city});
-
-  const response = await fetch(`http://localhost:5004/api/weather?${params}`);
-  const weatherData = await response.json();
-
-  console.log(weatherData);
-  setResult(weatherData); //<-- this means, result = weatherData;
-  }
-  
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    loadCity(city);
-  }
   return (
-  <>
-<h2> Weather in </h2>
-  <div className="weather-form">
-      <input
-          type="text"
-          id="city"
-          name="city"
-          placeholder="Enter city name"
-          onChange={(e) => setCity(e.target.value)} >
-      </input>
-      <button className="btn" onClick={ handleSubmit }>
-      Submit
-      </button>
-      <div>
-        {!result ? null : <WeatherCard data={result} />}
-      </div>
-  </div>
-  </>
-    );
+    <div>
+      <WeatherForm />
+      <WeatherCard />
+    </div>
+  );
 }
 export default App;
--- client/src/components/WeatherForm.jsx
+++ client/src/components/WeatherForm.jsx
@@ -1,39 +1,64 @@
 import { useState } from "react";
+import WeatherCard from "./WeatherCard";
 
-
-const WeatherForm = (props) =>{
-
+const WeatherForm = (props) => {
   const [city, setCity] = useState("");
+  const [data, setData] = useState(null);
 
-  const onChange = (ev) =>{
-    setCity(ev.target.value);
-  }
+  //fetch weather data from BE
+  const loadWeather = async (city) => {
+    const params = new URLSearchParams({ cityName: city });
+    const response = await fetch(`http://localhost:5004/api/weather?${params}`);
+    const weatherData = await response.json();
 
-  const handleSubmit = (ev) => {
-    ev.preventDefault();
-    props.handleSubmit(city);
-  }
+    setData(weatherData);
+  };
 
+  // const onChange = (ev) => {
+  //   setCity(ev.target.value);
+  // };
 
-    return (
-        <div className="weather">
-        <h1 className="App-header"> Weather App </h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            id="city-name"
-            type="text"
-            placeholder="Please enter the city name"
-            name="city"
-            value={city}
-            onChange={onChange}
-            required
-          />
-          <input type="submit" value="Submit" />
-        </form>
+  // const handleSubmit = (ev) => {
+  //   ev.preventDefault();
+  //   props.handleSubmit(city);
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   loadCity(city);
+  // };
+
+  return (
+    <div>
+      <h2> Weather in </h2>
+      <div className="weather-form">
+        <input
+          type="text"
+          placeholder="Enter city name"
+          onChange={(e) => setCity(e.target.value)}
+        ></input>
+        <button className="btn" onClick={handleSubmit}>
+          Submit
+        </button>
+        <div>{!result ? null : <WeatherCard data={result} />}</div>
       </div>
-    )
-}
+    </div>
+    // <div className="weather">
+    //   <h1 className="App-header"> Weather App </h1>
+    //   <form onSubmit={handleSubmit}>
+    //     <input
+    //       id="city-name"
+    //       type="text"
+    //       placeholder="Please enter the city name"
+    //       name="city"
+    //       value={city}
+    //       onChange={onChange}
+    //       required
+    //     />
+    //     <input type="submit" value="Submit" />
+    //   </form>
+    // </div>
+  );
+};
 
 export default WeatherForm;
-
-
